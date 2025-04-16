ffdd3a1aa01a5d5851ed90dac0de99c98e761904
Aniket Talwekar <talwekar.aniket@outlook.com>
2025-04-16 05:46:19 +0530

    Delete terraform/variables.tf

--- terraform/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "AWS Region to deploy resources"
-  default     = "ap-south-1"  # Mumbai Region
-}
-
-variable "instance_type" {
-  description = "EC2 instance type"
-  default     = "t2.micro"  # EC2 instance type
-}
