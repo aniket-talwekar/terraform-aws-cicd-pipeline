b8adf20b26cd0d7278177fe302280e8f745c4ae8
Aniket Talwekar <talwekar.aniket@outlook.com>
2025-04-16 05:44:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,7 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-080b1a55a0ad28c02"  # AMI ID for 64-bit (x86)
-  instance_type = var.instance_type
-
-  tags = {
-    Name = "Terraform-EC2-Instance"
-  }
-}
-
 output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.my_instance.public_ip
 }
 
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.my_instance.id
 }
